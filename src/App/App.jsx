6e12dfcce405f5726227edadb4b3e032f68f75e8
Denis-Ozolin <ozolindenx@gmail.com>
2022-05-09 22:26:49 +0300

    added title & descr.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,19 +6,19 @@ const links = [
     id: 1,
     name: 'Перейти до сайту',
     link: 'https://sweetvillage.metro.biz/?lang=uk',
-    description: 'Подивитись фото нашої продукції'
+    purpose: 'Подивитись фото нашої продукції'
   },
   {
     id: 2,
     name: 'Перейти у Телеграм',
     link: 'https://t.me/SweetVillage_bot?start=ZGw6OTIxOTc',
-    description: 'Отримати прайси для співпраці'
+    purpose: 'Отримати прайси для співпраці'
   },
   {
     id: 3,
     name: 'Зателефонувати',
     link: 'tel:+380634725349',
-    description: "Зв'язатись з нами"
+    purpose: "Зв'язатись з нами"
   },
 ];
 
@@ -26,10 +26,12 @@ function App() {
   return (
     <div className={css.app}>
       <div className={css.container}>
+        <h1 className={css.title}>Sweet Village</h1>
+        <p className={css.description}>Київське кондитерське виробництво</p>
         <ul className={css.itemsContainer}>
-          {links.map(({id, name, link, description}) => 
+          {links.map(({id, name, link, purpose}) => 
             <li key={id} className={css.item}>
-              <p className={css.description}>{description}</p>
+              <p className={css.purpose}>{purpose}</p>
               <Button name={name} link={link}/>
             </li>
           )}
